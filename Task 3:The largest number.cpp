4b0510d3cec5e44c859197aa9b7ca8012ab37de1
Rabina29 <mstrabinabegum029@gmail.com>
2024-09-24 09:38:53 -0700

    Add Create Task 3:The largest number.cpp

--- Task 3:The largest number.cpp
+++ Task 3:The largest number.cpp
@@ -4,21 +4,16 @@ int n1,n2,n3;
 printf("Enter three number:\n");
 scanf("%d%d%d",&n1,&n2,&n3);
 if(n1>=n2 && n1>=n3)
+{
     printf("The largest number is:%d\n",n1);
+}  
 else if(n2>=n1 && n2>=n3)
+{  
     printf("The largest number is:%d\n",n2);
+}
 else
+{
     printf("The largest number is :%d\n",n3);
-
-
-
-
-
-
-
-
-
-
+}
 return 0;
-
 }

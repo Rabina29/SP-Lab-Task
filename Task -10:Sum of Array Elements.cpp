92f0233da157d220cd42c22eb3d2bb568d378571
Rabina29 <mstrabinabegum029@gmail.com>
2024-09-27 19:56:02 -0700

    Add Create Task -10:Sum of Array Elements.cpp

---
+++ Task -10:Sum of Array Elements.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int size;
+printf("Enter size:");
+scanf("%d",&size);
+int arr[size],sum=0;
+for(int i=0;i<size;i++){
+ scanf("%d",&arr[i]);
+ sum=sum+arr[i];
+
+}
+printf("Total Sum=%d\n",sum);
+return 0;
+}
